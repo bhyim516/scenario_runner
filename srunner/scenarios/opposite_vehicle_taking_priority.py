c0a161c64256f9d576ddb0573a85b23ad1bcf2a6
Fabian Oboril <fabian.oboril@intel.com>
2019-03-29 20:53:06 +0100

    Adapt to WaypointFollower changes
    Change-Id: Iaeb9fecac08e36cb2b45bad45c330201cf038869

--- srunner/scenarios/opposite_vehicle_taking_priority.py
+++ srunner/scenarios/opposite_vehicle_taking_priority.py
@@ -131,9 +131,8 @@ class OppositeVehicleRunningRedLight(BasicScenario):
 
         sync_arrival = SyncArrival(
             self.other_actors[0], self.ego_vehicle, location_of_collision_dynamic)
-        sync_arrival_stop = InTriggerDistanceToVehicle(self.other_actors[0],
-                                                       self.ego_vehicle,
-                                                       41)
+        sync_arrival_stop = InTriggerDistanceToNextIntersection(self.other_actors[0],
+                                                                5)
         sync_arrival_parallel.add_child(sync_arrival)
         sync_arrival_parallel.add_child(sync_arrival_stop)
 
@@ -159,7 +158,7 @@ class OppositeVehicleRunningRedLight(BasicScenario):
             policy=py_trees.common.ParallelPolicy.SUCCESS_ON_ONE)
 
         continue_driving_waypoints = WaypointFollower(
-            self.other_actors[0], self._other_actor_target_velocity, plan=plan)
+            self.other_actors[0], self._other_actor_target_velocity, plan=plan, avoid_collision=False)
 
         continue_driving_distance = DriveDistance(
             self.other_actors[0],
